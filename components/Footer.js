b2dba71acf45a043f2048aa3ab2afbdaea20f9b5
adnaz <adnanaitzait@gmail.com>
2022-04-05 17:38:47 +0000

    link to tunkeyisp

--- components/Footer.js
+++ components/Footer.js
@@ -19,7 +19,7 @@ export default function Footer() {
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
-          <Link href="/">{siteMetadata.title}</Link>
+          <Link href="https://turnkeyisp.co">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           {/* <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
